e8da0ce91ee7df333c1a1d38971f5e1ea3451b8d
Eshan2004guna <eshan123guna@gmail.com>
2025-11-29 12:23:01 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,36 @@
+void main() {
+  print("eshan");
+  print("eshan induwara");
+  print("100");
+  print("100"+"200");
+  print(100+200);
+  print("------");
+  
+//   single line comment
+  /*  multi comment*/
+  
+  
+//  arithmatic operators
+  int a=10;
+  print(a);
+  a=a+1;
+  print(a);
+  
+//   prefix operators
+  int b=20;
+//   prefix increment 
+ print(++b);
+// prefix decrement
+  print(--b);
+ 
+ print("------");
+  
+//  postfix operators
+  //increment , in this one add 1 for current value but it will apply on next line. mean this line will not display new value after that c use new value
+ int c=30;
+  print(c++);
+  print(c);
+//   postfix decrement
+  print(c--);
+  print(c);
+}
